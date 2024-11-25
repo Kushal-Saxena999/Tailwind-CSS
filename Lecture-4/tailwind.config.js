36369c8b4bcd6d436c4aa0fc498cff093f9a0bcc
Kushal-Saxena999 <saxenakushal924@gmail.com>
2024-11-25 05:59:31 +0530

    Adding files to Tailwind

---
+++ Lecture-4/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    colors : {
+      "pink":"#00f10f"
+    },
+    fontWeight:{
+      "xl":900
+    },
+    extend: {
+      fontSize : {
+        "10xl" :"20rem"
+      }
+    },
+  },
+  plugins: [],
+}
+
